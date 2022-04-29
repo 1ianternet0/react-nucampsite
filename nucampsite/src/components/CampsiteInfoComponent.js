4ff9adc40755fdf003b06bb457e38c0d24accb88
ian baysa <ian_y.baysa@yahoo.com>
2022-04-28 19:01:43 -0700

    Redux Actions

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -21,6 +21,7 @@ class CommentForm extends Component {
     }
     handleSubmit(value) {
         this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
         console.log(`Current state is :${JSON.stringify(value)}`)
         alert(`Current State is: ${JSON.stringify(value)}`);
         
@@ -91,7 +92,7 @@ function RenderCampsite({campsite}) {
         )
     }
 
-    function RenderComments({comments}) {
+    function RenderComments({comments, addComment, campsiteId}) {
         if (comments) {
             return (
                 <div className="md-5">
@@ -110,7 +111,7 @@ function RenderCampsite({campsite}) {
                                 </div>
                             );
                         })}
-                        <CommentForm/> 
+                        <CommentForm campsiteId={campsiteId} addComment={addComment} /> 
                 </div>
             );
         }
@@ -134,7 +135,9 @@ function RenderCampsite({campsite}) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments} 
+                    addComment={props.addComment}
+                    campsiteId={props.campsite.id}/>
                 </div>
             </div>
             );
